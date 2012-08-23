e42488b3a2f3742a4205badb32e7477079a65a07
bfahy <bfahy@andrew.cmu.edu>
2012-08-22 22:42:42 -0400

    Cleaned up main

--- main.py
+++ main.py
@@ -1,79 +1,60 @@
 #!/usr/bin/env python
 
-#import tools
 import plot
-#import readinput
-#import indexes
 import configtimeobj
-import read_config_time_file
+import read_config_time_file as read
 import correlator
 
-#import cProfile
 
 emass_dts = range(2,5)
 
-srcop = "a1pp_0_optype0_op1"
-
+srcop = "a1pp_0_optype1_op1"
 snkop = "a1pp_0_optype0_op1"
 
+# srcop = "a1mp_0_optype6_op1"
+# snkop = "a1mp_0_optype6_op1"
 
-name = "%s,%s"%(srcop,snkop)
+if snkop == srcop:
+    name = "%s"%(srcop)
+else:
+    name = "%s.%s"%(srcop,snkop)
 
-folder = "/home/bfahy/data/michael/"
+data_folder = "/home/bfahy/r3/results/michael12beta5.9/data/"
 
-srcdata = read_config_time_file.read_config_time_data_real("%sopvals_%s.orthog_test1.dat"%(folder,srcop))
-
-#snkdata = read_config_time_file.read_config_time_data_real("%sopvals_%s.orthog_test1.dat"%(folder,snkop))
-snkdata = srcdata
-
-#srcdata = readinput.reduce_to_first_eig_val(rawsnkdata.data)
-#snkdata = readinput.reduce_to_first_eig_val(rawsrcdata.data)
-
-corr = correlator.Correlator.fromOpvalCTO(srcdata,snkdata,dts=range(0,8))
-
-# corr.writefullcorrfile("/home/bfahy/plots/testing/single")
-# corr.writeeachconfig("/home/bfahy/plots/testing/correach")
-
-#print corr.average_over_configs()
-# corr.subvev()
-# print "subtracting vevs"
-# print corr.average_over_configs()
-# print "vevs"
-# print corr.vevs()[1]
-# print corr.average_vev()
-# print corr.op1.average_vev()
-# print corr.op2.average_vev()
-
-# print {t: c - corr.op1.average_vev()*corr.op2.average_vev() for t,c in corr.average_over_configs().iteritems()}
-
-# print corr.average_sub_vev()
-# print "JK"
-# print corr.jackknife_average_sub_vev()
-# print corr.jackknifed_errors()
-#exit()
-
-# cProfile.run('correlator.Correlator.fromOpvalCTO(srcdata,snkdata)',"profile_build.out")
-# cProfile.run('corr.average_sub_vev()',"profile_avg_sub_vev.out")
-# cProfile.run('corr.jackknifed_errors()',"profile_jack.out")
-# exit()
-
-avgcorr = corr.average_sub_vev()
-corr_errors = corr.jackknifed_errors()
-
-plot_corr = {"%s, \t error" %name : (avgcorr, corr_errors)}
-
-prefix = "testing"
-plot.plotwitherrorbarsnames("../plots/%s/corrilator" % prefix, plot_corr, avgcorr.keys() ,autoscale=True)
-
-for dt in emass_dts:
-    emass = corr.effective_mass(dt)
-    emass_errors = corr.effective_mass_errors(dt)
-    plot_emass = {"emass dt=%d, \t error" %dt : (emass, emass_errors)}
-    plot.plotwitherrorbarsnames("../plots/%s/emass%d" % (prefix,dt), plot_emass, emass.keys() ,autoscale=True)
-
-
-#cProfile.run('corr.average_sub_vev()',"profile_avg_sub_vev.out")
+out_folder ="/home/bfahy/plots/testing/"
 
 
 
 
+def compute_and_plot(srcop,snkop,data_folder,out_folder,name):
+
+
+    srcdata = read.read_config_time_data_real("%sopvals_%stest1.dat"%(data_folder,srcop))
+    if srcop==snkop:
+        snkdata = srcdata
+    else:
+        snkdata = read.read_config_time_data_real("%sopvals_%stest1.dat"%(data_folder,snkop))
+
+    corr = correlator.Correlator.fromOpvalCTO(srcdata,snkdata)
+
+    # corr.writefullfile("/home/bfahy/plots/testing/corr/%s" % name)
+    
+    avgcorr = corr.average_sub_vev()
+    corr_errors = corr.jackknifed_errors()
+    
+    plot_corr = {"%s, \t error" %name : (avgcorr, corr_errors)}
+
+    plot.plotwitherrorbarsnames("%scorrelator%s" % (out_folder,name),
+                                plot_corr, avgcorr.keys() ,autoscale=True)
+
+    for dt in emass_dts:
+        emass = corr.effective_mass(dt)
+        emass_errors = corr.effective_mass_errors(dt)
+        plot_emass = {"%s emass dt=%d, \t error" %(name,dt) : ( emass, emass_errors)}
+        plot.plotwitherrorbarsnames("%semass%d.%s" % (out_folder,dt,name), plot_emass,
+                                    emass.keys() ,autoscale=True)
+
+    print "done with %s %s to %s" % (srcop,snkop,out_folder)
+
+
+compute_and_plot(srcop,snkop,data_folder,out_folder,name)
