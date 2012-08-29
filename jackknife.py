7ce5b727800e288d3c73119e289088cc163bef36
bfahy <bfahy@andrew.cmu.edu>
2012-08-29 14:18:56 -0400

    conform to pylint

--- jackknife.py
+++ jackknife.py
@@ -1,5 +1,6 @@
 import math
 
+
 def jackknife_errors(data, datajk):
     """Compute the error bars from the data and the data with specific
     configs removed"""
@@ -8,16 +9,17 @@ def jackknife_errors(data, datajk):
 
         total = 0.0
         M = 0
-        for cfg, val in datajk[shift].iteritems():
+        for _, val in datajk[shift].iteritems():
             total += ((data[shift]) - (val)) ** 2
             M += 1
         fm = float(M)
         errors[shift] = math.sqrt(((fm - 1) / (fm)) * total)
     return errors
 
+
 def errorbars(data, datajk):
     #print [ (data - datajk[cfg]) ** 2 for cfg in datajk.keys() ]
-    total = math.fsum( ((data - datajk[cfg]) ** 2) for cfg in datajk.keys() )
+    total = math.fsum(((data - datajk[cfg]) ** 2) for cfg in datajk.keys())
     fm = float(len(datajk))
     #print "FM",fm
     #print total
