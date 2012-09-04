454db3eda82eae5876c4a7180fdd8671c0c3044c
bfahy <bfahy@andrew.cmu.edu>
2012-09-04 15:28:20 -0400

    Updated main to be a bit better

--- main.py
+++ main.py
@@ -6,22 +6,27 @@ import read_config_time_file as read
 import correlator
 
 
-emass_dts = range(2,5)
+emass_dts = range(1,4)
 
-srcop = "a1pp_0_optype1_op1"
-snkop = "a1pp_0_optype0_op1"
+# srcop = "a1pp_0_optype0_op1"
+# snkop = "a1pp_0_optype0_op1"
 
 # srcop = "a1mp_0_optype6_op1"
 # snkop = "a1mp_0_optype6_op1"
 
-if snkop == srcop:
-    name = "%s"%(srcop)
-else:
-    name = "%s.%s"%(srcop,snkop)
 
-data_folder = "/home/bfahy/r3/results/michael12beta5.9/data/"
+# if snkop == srcop:
+#     name = "%s"%(srcop)
+# else:
+#     name = "%s.%s"%(srcop,snkop)
 
-out_folder ="/home/bfahy/plots/testing/"
+#ops = ["a1pp_0_optype0_op1","a1pp_0_optype10_op1","a1pp_0_optype1_op1"]
+ops = ["a1pp_0_optype0_op1","a1pp_0_optype10_op1"]
+
+
+data_folder = "/home/bfahy/r3/results/rngtest2_smear0.17/data/"
+
+out_folder ="/home/bfahy/glueballstesting/rngtest2/"
 
 
 
@@ -35,7 +40,7 @@ def compute_and_plot(srcop,snkop,data_folder,out_folder,name):
     else:
         snkdata = read.read_config_time_data_real("%sopvals_%stest1.dat"%(data_folder,snkop))
 
-    corr = correlator.Correlator.fromOpvalCTO(srcdata,snkdata)
+    corr = correlator.Correlator.fromOpvalCTO(srcdata,snkdata,dts=list(range(9)))
 
     # corr.writefullfile("/home/bfahy/plots/testing/corr/%s" % name)
     
@@ -57,4 +62,6 @@ def compute_and_plot(srcop,snkop,data_folder,out_folder,name):
     print "done with %s %s to %s" % (srcop,snkop,out_folder)
 
 
-compute_and_plot(srcop,snkop,data_folder,out_folder,name)
+for op in ops:
+    name = op
+    compute_and_plot(op,op,data_folder,out_folder,name)
