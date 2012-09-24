2d4482593264ffd82fde8b6f626e8152ec9d96bb
Brendan Fahy <bmfahy@gmail.com>
2012-09-24 15:29:30 -0400

    Cleanup to pass pylint and pep8

--- eigenvalues.py
+++ eigenvalues.py
@@ -3,14 +3,12 @@ time step \t lambda0 \t lambda1 \t lambda2 ...
 """
 import os
 import numpy as np
-import math
 import logging
-
-#from scipy import linalg
 import cPickle as pickle
 
 #DEBUG = True
 DEBUG = False
+
 
 def readfile_neigenvalues(basedir, N):
     """reads datafile, returns data object
@@ -18,7 +16,6 @@ def readfile_neigenvalues(basedir, N):
     data[configuration file][time step][ith eigen value]
     """
     data = {}
-    config = {}
 
     assert N > 0, "must give at least one eigenvalue"
 
@@ -27,17 +24,18 @@ def readfile_neigenvalues(basedir, N):
 
     if DEBUG:
         files = files[:10]
-    
+
     for filename in [x for x in files if x[-3:] == "dat"]:
         f = open(basedir + filename)
 
-        rawdata = np.loadtxt(f, comments="#", usecols=(tuple(range(1, N+1))))
+        rawdata = np.loadtxt(f, comments="#", usecols=(tuple(range(1, N + 1))))
         data[filename] = dict(enumerate(rawdata))
 
         logging.debug("read %s", filename)
 
     logging.info("done reading files")
     return data
+
 
 def readfullfile_config_time(filename):
     """Read-in chimera formated corrilator files
@@ -48,21 +46,21 @@ def readfullfile_config_time(filename):
     """
 
     logging.info("reading chimera formated file %s\n", filename)
-    
+
     f = open(filename)
 
-    names = ["time","real","imag"]
+    names = ["time", "real", "imag"]
 
     header = f.readline()       # e.g. 100 40 1 0 1
-    numberofconfigs,numberoftimes,_,_,_ = map(int,header.split())
+    numberofconfigs, numberoftimes, _, _, _ = map(int, header.split())
 
     f.seek(0)                   # Go back to the start
 
-    rawdata = np.genfromtxt(f, comments="#", skiprows=1, dtype=None, names = names )
+    rawdata = np.genfromtxt(f, comments="#", skiprows=1, dtype=None, names=names)
 
     data = {}
-    for confnumber,config in enumerate(rawdata.reshape(numberofconfigs,numberoftimes)):
-        data[confnumber] = dict({time:complex(real,imag) for time,real,imag in config})
+    for confnumber, config in enumerate(rawdata.reshape(numberofconfigs, numberoftimes)):
+        data[confnumber] = dict({time: complex(real, imag) for time, real, imag in config})
 
     return data
 
@@ -79,23 +77,20 @@ def read_configdir_time_evalues(dirname, N=-1):
     """
     assert N > 0, "must give at least one eigenvalue"
 
-    
     data = {}
     configdata = {}
 
-    logging.debug("Reading from %s",(os.path.relpath(dirname)))
-    
+    logging.debug("Reading from %s", (os.path.relpath(dirname)))
+
     # Hopfully no files that are digits!
     configs = [x for x in os.listdir(dirname) if x.isdigit()]
-    logging.debug("found configs ",configs)
+    logging.debug("found configs ", configs)
 
-    
     for config in configs:
-    
-        files = list(os.listdir(dirname+config))
-        
-        
-        filebasename = files[0].split('.')[0] 
+
+        files = list(os.listdir(dirname + config))
+
+        filebasename = files[0].split('.')[0]
 
         # if DEBUG:
         #     files = files[:128]
@@ -105,21 +100,20 @@ def read_configdir_time_evalues(dirname, N=-1):
 
         times = range(len(files))
 
-        configdata ={}
+        configdata = {}
         for time in times:
 
-            filepath=dirname + config + "/" + filebasename+ '.' + str(time)
+            filepath = dirname + config + "/" + filebasename + '.' + str(time)
             f = open(filepath)
 
-            
-            
             rawdata = np.loadtxt(f, comments="#")
             configdata[time] = rawdata[0:N]
-            logging.info("Done reading file %s" %filepath)
+            logging.info("Done reading file %s", filepath)
 
         data[config] = configdata
     return data
-    
+
+
 def read_configdir_timeorlevel_evalues(dirname, N=-1, store=True, recall=True):
     """Read in eigenvalues from a directory where the directory
     represents a config.
@@ -143,28 +137,25 @@ def read_configdir_timeorlevel_evalues(dirname, N=-1, store=True, recall=True):
     if(recall):
         try:
             logging.info("opening pickle")
-            data= pickle.load( open( dirname + "save.p", "rb" ) )
+            data = pickle.load(open(dirname + "save.p", "rb"))
             return data
-        except IOError as e:
+        except IOError:
             logging.error("no picked file, building from scratch")
-                
 
     data = {}
     configdata = {}
 
-    logging.debug("Reading from %s",(os.path.relpath(dirname)))
+    logging.debug("Reading from %s", (os.path.relpath(dirname)))
     # Hopfully no files that are digits!
     configs = [x for x in os.listdir(dirname) if x.isdigit()]
 
     logging.info(configs)
-    # configs = ['1000','1220']
-    
+    # configs = ['1000', '1220']
+
     for config in configs:
-        
-        files = list(os.listdir(dirname+config))
-        
-        
-        filebasename = files[0].split('.')[0] 
+        files = list(os.listdir(dirname + config))
+
+        filebasename = files[0].split('.')[0]
 
         # if DEBUG:
         #     files = files[:128]
@@ -175,45 +166,42 @@ def read_configdir_timeorlevel_evalues(dirname, N=-1, store=True, recall=True):
         if("time" in filebasename):
             logging.debug("Config %s is in time format", config)
             times = range(len(files))
-            configdata ={}
+            configdata = {}
             for time in times:
-                filepath=dirname + config + "/" + filebasename+ '.' + str(time)
+                filepath = dirname + config + "/" + filebasename + '.' + str(time)
                 f = open(filepath)
-                
+
                 rawdata = np.loadtxt(f, comments="#")
                 configdata[time] = rawdata[0:N]
 
         elif("level" in filebasename):
             logging.debug("Config %s is in level format", config)
             levels = range(len(files))
-            
+
             rawdata = None
             for level in levels[0:N]:
-                filepath=dirname + config + "/" + filebasename+ '.' + str(level)
-                logging.debug("reading file %s" %filepath )
+                filepath = dirname + config + "/" + filebasename + '.' + str(level)
+                logging.debug("reading file %s", filepath)
                 f = open(filepath)
-                
+
                 readdata = np.loadtxt(f, comments="#")
                 if rawdata is not None:
-                    rawdata = np.column_stack((rawdata,readdata))
+                    rawdata = np.column_stack((rawdata, readdata))
                 else:
                     rawdata = readdata
 
-                
             configdata = dict(enumerate(rawdata))
-            
+
         else:
             raise TypeError("unsure which format, exiting")
-            
-
 
         data[config] = configdata
     if(store):
-        pickle.dump( data, open( dirname+"save.p", "wb" )  )
+        pickle.dump(data, open(dirname + "save.p", "wb"))
         logging.info("stored data as %s save.p", dirname)
     return data
-    
-    
+
+
 def reduce_to_first_eig_val(data):
     newdata = {}
     for config, values in data.iteritems():
@@ -223,6 +211,7 @@ def reduce_to_first_eig_val(data):
         newdata[config] = newconfvalue
 
     return newdata
+
 
 def reduce_to_trace(data):
     newdata = {}
@@ -234,7 +223,8 @@ def reduce_to_trace(data):
 
     return newdata
 
-def reduce_to_trace_n_n(data,n,m):
+
+def reduce_to_trace_n_n(data, n, m):
     newdata = {}
     for config, values in data.iteritems():
         newconfvalue = {}
@@ -244,7 +234,7 @@ def reduce_to_trace_n_n(data,n,m):
 
     return newdata
 
-    
+
 def reduce_to_exponentiate_trace(data):
     newdata = {}
     for config, values in data.iteritems():
@@ -256,9 +246,8 @@ def reduce_to_exponentiate_trace(data):
             newdata[config] = newconfvalue
 
     return newdata
-    
-    
-    
+
+
 def reduce_to_nth_eig_val(data, n):
     newdata = {}
     for config, values in data.iteritems():
@@ -268,11 +257,6 @@ def reduce_to_nth_eig_val(data, n):
         newdata[config] = newconfvalue
 
     return newdata
-
-
-
-
-
 
 
 def diagonalize(data):
@@ -285,41 +269,39 @@ def diagonalize(data):
 
     return newdata
 
-"""test readinput"""
 if __name__ == "__main__":
+    """test readinput"""
+    pass
+#     testdir = "./alleigens860/"
+#     data = readfile_neigenvalues(testdir, 1)
+#     import tools
+#     x, trash = tools.computecors(data, 1)
+#     #print(x)
+#     raise SystemExit
+#     testarray = np.ones(1)
+#     time = 71
+#     t_o = 7
+#     for config, values in data.iteritems():
+#         testarray = {}
+#         testarray[t_o] = values[t_o]
+#         testarray[time] = values[time]
+# #         for time, array in values.iteritems():
+# #             testarray = array
+# # #            break
+#         break
+# #    testarray = testarray[:2]
+#     #print(testarray[t_o])
+#     #print(testarray[time])
+#     M= np.mat(np.outer(testarray[t_o], testarray[time]))
+#     #print("M")
+#     lamb, evec = np.linalg.eig(M)
+#     # print "evec", len(evec)
+#     # print evec
 
-    testdir = "./alleigens860/"
-    data = readfile_neigenvalues(testdir, 1)
-    import tools
-    x, trash = tools.computecors(data, 1)
-    #print(x)
-    raise SystemExit
-    testarray = np.ones(1)
-    time = 71
-    t_o = 7
-    for config, values in data.iteritems():
-        testarray = {}
-        testarray[t_o] = values[t_o]
-        testarray[time] = values[time]
-#         for time, array in values.iteritems():
-#             testarray = array
-# #            break
-        break
-#    testarray = testarray[:2]
-    #print(testarray[t_o])
-    #print(testarray[time])
-    M= np.mat(np.outer(testarray[t_o], testarray[time]))
-    #print("M")
-    lamb, evec = np.linalg.eig(M)
-    # print "evec", len(evec)
-    # print evec
-
-    #print "eigenvalues", lamb
-    # print evec[0]
-    # print np.vdot(evec[0], evec[0])
-    # e1= evec[0]/math.sqrt(np.vdot(evec[0], evec[0]))
-    # print e1
-    # print np.vdot(e1, e1)
-    # print np.linalg.inv(evec) * (M * evec)
-
-
+#     #print "eigenvalues", lamb
+#     # print evec[0]
+#     # print np.vdot(evec[0], evec[0])
+#     # e1= evec[0]/math.sqrt(np.vdot(evec[0], evec[0]))
+#     # print e1
+#     # print np.vdot(e1, e1)
+#     # print np.linalg.inv(evec) * (M * evec)
