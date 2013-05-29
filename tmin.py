093e7f5843c376e82e06fd31b5f38080d509de52
Brendan Fahy <bmfahy@gmail.com>
2013-05-29 17:54:59 -0400

    both plots now on the same plot, and can toggle them

--- tmin.py
+++ tmin.py
@@ -5,6 +5,8 @@ import logging
 import build_corr
 import argparse
 import fit
+from matplotlib.widgets import CheckButtons
+from compiler.ast import flatten
 
 from fitfunctions import *  # noqa
 import inspect
@@ -30,21 +32,39 @@ def tmin_plot(fn, cor, tmin, tmax, filename=None, bootstraps=NBOOTSTRAPS):
 
     emass = cor.effective_mass(emass_dt)
     emass_errors = cor.effective_mass_errors(emass_dt).values()
-    emassplot = plt.subplot(211)
-    emassplot.errorbar(emass.keys(), emass.values(), yerr=emass_errors, fmt='o')
+    emass_plot = plt.errorbar(np.array(emass.keys())+0.2, emass.values(), yerr=emass_errors, fmt='g^', zorder=0)
+    cmap = mpl.cm.cool
+
+    tmin_plot = plt.scatter(range(tmin, tmax-1), fitted_params, c=qualities, s=50, cmap=cmap)
+    tmin_error = plt.errorbar(range(tmin, tmax-1), fitted_params, yerr=fitted_errors, fmt=None, zorder=0)
+
+    for i in flatten(emass_plot):
+        i.set_visible(False)
+
+    fig.colorbar(tmin_plot)
+
     plt.ylim([0, max(emass.values())*1.2])
     plt.xlim([0, tmax + 2])
 
-    tminplot = plt.subplot(212)
-    cmap = mpl.cm.cool
-    s = tminplot.scatter(range(tmin, tmax-1), fitted_params, c=qualities, s=50, cmap=cmap)
-    tminplot.errorbar(range(tmin, tmax-1), fitted_params, yerr=fitted_errors, fmt=None, zorder=0)
-    plt.ylim([0, max(emass.values())*1.2])
-    plt.xlim([0, tmax + 2])
-    fig.colorbar(s)
+    rax = plt.axes([0.85, 0.8, 0.1, 0.15])
+    check = CheckButtons(rax, ('tminplot', 'emass'), (True, False))
+
+    def func(label):
+        if label == 'tminplot':
+            tmin_plot.set_visible(not tmin_plot.get_visible())
+            for i in flatten(tmin_error):
+                if i:
+                    i.set_visible(not i.get_visible())
+        elif label == 'emass':
+            for i in flatten(emass_plot):
+                if i:
+                    i.set_visible(not i.get_visible())
+        plt.draw()
+
     if(filename):
         plt.savefig(filename)
     else:
+        check.on_clicked(func)
         plt.show()
 
 
