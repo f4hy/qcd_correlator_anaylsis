66105d45350e23801fa617527f4492f4b6014f39
Brendan Fahy <bmfahy@gmail.com>
2013-06-20 14:27:00 -0400

    better two exp fit function

--- fitfunctions.py
+++ fitfunctions.py
@@ -47,13 +47,31 @@ class periodic_exp:
 
 class two_exp:
     def __init__(self, **kargs):
-        self.starting_guess = [0.5, 100, 1.0, 10]
+        self.starting_guess = [0.05, 100, 1.0, 10]
         self.parameter_names = ["mass", "amp", "mass2", "amp2"]
         self.description = "two_exp"
-        self.template = "{1: f}exp(-{0: f}*t)+{3: f}exp(-{2: f}*t)"
+        self.template = "{1: f}exp(-{0: f}*t)(1+{3: f}exp(-{2: f}^2*t)"
 
     def formula(self, v ,x):
-        return (v[1] * np.exp((-1.0) * v[0] * x))+(v[3] * np.exp((-1.0) * v[2] * x))
+        return (v[1] * np.exp((-1.0) * v[0] * x)*(1.0+ v[3]*exp((-1.0)*v[2]*x) ))
+
+class periodic_two_exp:
+    def __init__(self, Nt=None):
+        self.starting_guess = [0.05, 100, 1.0, 10]
+        self.parameter_names = ["mass", "amp", "mass2", "amp2"]
+        self.description = "periodic_two_exp"
+        self.template = "{1: f}exp(-{0: f}*t)(1+{3: f}exp(-{2: f}^2*t)"
+        self.Nt = Nt
+        while not self.Nt:
+            try:
+                self.Nt=int(raw_input('Time period not specified, please enter Nt:'))
+            except ValueError:
+                print "Not a valid number"
+
+    def formula(self, v ,x):
+                return ((v[1] * np.exp((-1.0) * v[0] * x)*(1.0+ v[3]*np.exp((-1.0)*v[2]*x) ) )+
+                        (v[1] * np.exp(v[0] * (x-(self.Nt)))*(1.0+ v[3]*np.exp(v[2]*(x-(self.Nt))) )) )
+
 
 class jlab:
     def __init__(self, **kargs):
