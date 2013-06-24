a2925f6091bf04d8b5b42b391bf9ca9a7309a5e0
Brendan Fahy <bmfahy@gmail.com>
2013-06-24 12:17:01 -0400

    cleanup

--- level_identifier.py
+++ level_identifier.py
@@ -2,30 +2,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import logging
-import correlator
-import build_corr
-import pylab
 import argparse
-import os
-
-from fitfunctions import *  # noqa
-import inspect
-import sys
 import plot_files
 import pandas as pd
 
-from scipy import linalg
-from scipy.special import gammaincc
-from scipy.optimize import leastsq
-from scipy.optimize import fmin
-# from scipy.optimize import minimize
 OUTPUT = 25
 logging.addLevelName(OUTPUT, "OUTPUT")
 
 
 def read_file(filename):
     txt = plot_files.lines_without_comments(filename)
-    parse_pair = plot_files.parse_pair
+    # parse_pair = plot_files.parse_pair
     # df = pd.read_csv(txt, delimiter=' ', names=["id", "identities", "error"],
     #                  converters={1: parse_pair, 2: parse_pair}, index_col=0)
     df = pd.read_csv(txt, delimiter=' ', names=["id", "identities", "error"], index_col=0)
@@ -36,7 +23,7 @@ def make_bar_plot(inputfile, cols, output_stub):
     df = read_file(inputfile).apply(np.absolute)
 
     N = int(np.sqrt(len(df)))
-    ops = range(1,N+1)
+    ops = range(1, N+1)
 
     plots = {}
     for op1 in ops:
@@ -44,9 +31,9 @@ def make_bar_plot(inputfile, cols, output_stub):
         j = (op1-1) % cols
         indexes = [op1+op2*1000 for op2 in ops]
         values = df.ix[indexes].identities.values
-        errors = df.ix[indexes].error.values
         ax = plt.subplot2grid((N/cols+1, cols), (i, j))
-        #plots[(i, j)] = ax.bar(ops, values, yerr=errors)
+        # errors = df.ix[indexes].error.values
+        # plots[(i, j)] = ax.bar(ops, values, yerr=errors)
         plots[(i, j)] = ax.bar(ops, values)
         plt.ylim([0, 1])
     if(output_stub):
