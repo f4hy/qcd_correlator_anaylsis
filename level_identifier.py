763ae0a933fbaeddef421d626030a4142f81ee93
Brendan Fahy <bmfahy@gmail.com>
2013-06-27 14:49:20 -0400

    added a second plot mode. Also added colors

--- level_identifier.py
+++ level_identifier.py
@@ -19,7 +19,8 @@ def read_file(filename):
     return df
 
 
-def make_bar_plot(inputfile, cols, output_stub):
+
+def make_bar_plot(inputfile, cols, output_stub, mode, ns):
     df = read_file(inputfile).apply(np.absolute)
 
     N = int(np.sqrt(len(df)))
@@ -30,12 +31,26 @@ def make_bar_plot(inputfile, cols, output_stub):
     for op1 in ops:
         i = (op1-1)/cols
         j = (op1-1) % cols
-        indexes = [op1+op2*1000 for op2 in ops]
-        values = df.ix[indexes].identities.values
+
         ax = plt.subplot2grid((N/cols+1, cols), (i, j))
+        indexes = [op1+op2*1000 for op2 in ops]
+        plt.xlabel("operator")
+        if mode == "ops":
+            indexes = [op1*1000+op2 for op2 in ops]
+            plt.xlabel("levels")
+        values = df.ix[indexes].identities.values
         # errors = df.ix[indexes].error.values
         # plots[(i, j)] = ax.bar(ops, values, yerr=errors)
-        plots[(i, j)] = ax.bar(ops, values)
+        if mode == "level":
+            plots[(i, j)] = ax.bar(ops[:ns], values[:ns], color="g")
+            plots[(i, j)] = ax.bar(ops[ns:], values[ns:], color="b")
+        else:
+            color = "b"
+            if op1 <= ns:
+                color = "g"
+            plots[(i, j)] = ax.bar(ops, values, color=color)
+
+
         plt.ylim([0, 1])
 
     plt.tight_layout()
@@ -56,6 +71,10 @@ if __name__ == "__main__":
                         help="stub of name to write output to")
     parser.add_argument("-c", "--columns", type=int, required=False,
                         help="number of columns to make the plot", default=2)
+    parser.add_argument("-m", "--mode", type=str, required=False, choices=["level", "ops"],
+                        help="select the mode of how to plot the levels", default="level")
+    parser.add_argument("-ns", "--number-singlehadrons", type=int, required=False, default=0,
+                        help="Number of single hadrons to distinguish")
     parser.add_argument("-v", "--verbose", action="store_true",
                         help="increase output verbosity")
 
@@ -66,4 +85,4 @@ if __name__ == "__main__":
     else:
         logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
-    make_bar_plot(args.inputfile, args.columns, args.output_stub)
+    make_bar_plot(args.inputfile, args.columns, args.output_stub, args.mode, args.number_singlehadrons)
