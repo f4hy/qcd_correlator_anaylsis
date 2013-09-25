ffd5f4bef6f6906db4602d97bd7e191d57774c2c
Brendan Fahy <bmfahy@gmail.com>
2013-09-25 18:26:06 -0400

    verified working diagonalziation. Cleaned up the code as well
    Had major error in swapping A and B. Now it works

--- diagonalize.py
+++ diagonalize.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
-import correlator
-import struct
-import build_corr
 import logging
 import numpy as np
 import pandas as pd
@@ -10,65 +7,89 @@ import determine_operators
 import argparse
 import pandas_reader
 
-DIAGTOL = 0.01
+DIAGTOL = 0.009
+
 
 def compare_matrix(A, B):
     logging.debug("comparing \n{a} to \n{b}".format(a=A, b=B))
-    normed_diff = min(LA.norm(A-B) , LA.norm(np.absolute(A)-B))
-    logging.debug("difference of {}".format(normed_diff))
-    return normed_diff
+    return np.allclose(A, B)
 
-def diagonalize(correlator_pannel, t0, ts):
+
+def hermitionize(M):
+    """Take a matrix, and return hermition one"""
+    return (M+M.H)/2.0
+
+
+def diagonalize(correlator_pannel, t0, td):
     length = correlator_pannel.shape[0]
     n = int(np.sqrt(length))
-    print n
-    print correlator_pannel.major_xs(3).values
     # Here we access the pannel major_xs gives time(n), mean incase it
     # was a multi correlator should have no effect on an already averaged one
-    A = np.matrix(np.reshape(correlator_pannel.major_xs(3).mean().values,(n,n)))
-    B = np.matrix(np.reshape(correlator_pannel.major_xs(5).mean().values,(n,n)))
-    logging.debug("A = {} \n B = {}".format(A,B))
+    A = np.matrix(np.reshape(correlator_pannel.major_xs(td).mean().values, (n, n)))
+    B = np.matrix(np.reshape(correlator_pannel.major_xs(t0).mean().values, (n, n)))
+    # Require A and B to be hermition for our generalized eigen value
+    # problem method to work. Here we force the matricies to be
+    # hermtion. This is justified since it is just useing the other
+    # measurement of the same value and averaging them.
+    A = hermitionize(A)
+    B = hermitionize(B)
+    logging.debug("A = {} \n B = {}".format(A, B))
 
-    evals, evecs = LA.eigh(A, b=B)
+    # Instead of using generalized eigen problem, we could solve a
+    # regular eigen problem involving Binvqrt
+    # Binvsqrt =  LA.inv(LA.sqrtm(B))
+    # evals, evecs = LA.eigh(Binvsqrt*A*Binvsqrt)
+    # evecs = np.matrix(evecs)
+    # V = np.matrix(Binvsqrt)*evecs
+
+    evals, evecs = LA.eigh(A, b=B)  # eigh works only if hermitian
     evecs = np.matrix(evecs)
     logging.debug("eigen values are {}".format(evals))
     logging.debug("eigen vectors are {}".format(evecs))
+    n = len(evecs)
+    logging.debug("Matrix size {N}x{N}".format(N=n))
 
     def rotate(x):
-        n = int(np.sqrt(len(x)))
-        logging.debug("Matrix size {N}x{N}".format(N=n))
-        M = np.matrix(np.resize(x,(n,n)))
+        M = np.matrix(np.resize(x, (n, n)))
+        #M = hermitionize(M)
         D = evecs.H * M * evecs
         R = np.array(D).flatten()
         return R
 
     diag = correlator_pannel.apply(rotate, "items")
 
-    assert (compare_matrix(np.reshape(diag.major_xs(ts).mean().values,(n,n)), np.identity(n)) < DIAGTOL), "Rotation error: is not ~identity at t-star"
-    assert (compare_matrix(np.reshape(diag.major_xs(t0).mean().values,(n,n)), np.diag(evals)) < DIAGTOL), "Rotation error: Is not ~Lambda at t0"
+    assert compare_matrix(np.reshape(diag.major_xs(t0).mean().values, (n, n)),
+                          np.identity(n)), "Rotation error: is not ~identity at t0"
+    assert compare_matrix(np.reshape(diag.major_xs(td).mean().values, (n, n)),
+                          np.diag(evals)), "Rotation error: Is not ~Lambda at td"
 
     return diag
 
+
 def parenformat(x):
     return "({},{})".format(np.real(x), np.imag(x))
+
+
 def parenformatn(x):
     return ["({},{})".format(np.real(i), np.imag(i)) for i in x]
+
 
 def write_cor_matrix(correlator_pannel, outputwild, ops, suffix=""):
     for snk in ops:
         for src in ops:
-            filename = outputwild.format(snk,src)+suffix
+            filename = outputwild.format(snk, src)+suffix
             first = True
-            if correlator_pannel[snk+src].ndim == 1:
-                correlator_pannel[snk+src].apply(parenformat).to_csv(filename,sep=" ", header=True, index_label="#time")
+            cor = correlator_pannel[snk+src]
+            if cor.ndim == 1:
+                cor.apply(parenformat).to_csv(filename, sep=" ", header=True, index_label="#time")
                 continue
-            for n in correlator_pannel[snk+src]:
+            for n in cor:
                 if first:
-                    correlator_pannel[snk+src][n].apply(parenformat).to_csv(filename,sep=" ", header=True, index_label="#time")
+                    cor[n].apply(parenformat).to_csv(filename, sep=" ", header=True, index_label="#time")
                     first = False
                 else:
-                    correlator_pannel[snk+src][n].apply(parenformat).to_csv(filename,sep=" ", header=False, mode="a")
-    logging.info("Wrote correlator matrix to {}{}".format(outputwild.format("SNK","SRC"),suffix))
+                    cor[n].apply(parenformat).to_csv(filename, sep=" ", header=False, mode="a")
+    logging.info("Wrote correlator matrix to {}{}".format(outputwild.format("SNK", "SRC"), suffix))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Diagonalize correlator matrix")
@@ -77,6 +98,10 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--filewild", type=str, required=True,
                         help="fromat of the correlator files in the directory\n\n"
                         "e.g. cor-snk{}_src{}.dat where {} are replaced with operator strings")
+    parser.add_argument("-ts", "--tstar", type=int, required=True,
+                        help="time diagonalization was done")
+    parser.add_argument("-to", "--tnaught", type=int, required=True,
+                        help="t naught, reference time")
     parser.add_argument("-r", "--operators", action='append', required=False,
                         help="operator to make e.g. -r etap000DDL7Egp1")
     parser.add_argument("-i", "--input-dir", type=str, required=True,
@@ -91,13 +116,12 @@ if __name__ == "__main__":
     else:
         logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
-
     if not args.operators:
         logging.info("Operators not specified, attempting to automagically determine")
         ops = determine_operators.matching_operators(args.input_dir, args.filewild)
         logging.info("Operators automagically found to be {}".format(",".join(ops)))
         if not ops:
-            print "Error: no operators found"
+            logging.error("Error: no operators found")
             parser.print_help()
             parser.exit()
         args.operators = ops
@@ -107,14 +131,14 @@ if __name__ == "__main__":
     cor_matrix_ave = {}
     for snk in args.operators:
         for src in args.operators:
-            filename = args.input_dir + args.filewild.format(snk,src)
+            filename = args.input_dir + args.filewild.format(snk, src)
             logging.info("reading {}".format(filename))
             cor_matrix[snk+src] = pandas_reader.read_configcols_paraenformat(filename)
 
     p = pd.Panel(cor_matrix)
-    diag = diagonalize(p, 3, 5)
+    diag = diagonalize(p, args.tnaught, args.tstar)
     diagave = diag.mean(2)
 
     if args.outputformat:
-        write_cor_matrix(diag, args.outputformat, args.operators)
+        write_cor_matrix(diag, args.outputformat, args.operators, suffix=".full")
         write_cor_matrix(diagave, args.outputformat, args.operators, suffix=".ave")
