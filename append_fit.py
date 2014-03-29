85ca41a9611eab5099f445b54773931147d7feb6
Brendan Fahy <bmfahy@gmail.com>
2014-03-28 20:12:51 -0400

    script to append fit result to the start of a file

---
+++ append_fit.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import logging
+import argparse
+import re
+
+
+def append_fit_results():
+
+    txt = args.fitlog.read()
+    ts, te = re.findall("t=.*(\d+) to (\d+)", txt)[0]
+    mass, masserror = re.findall("mass.*(\d+\.\d+).*(\d+\.\d+)", txt)[0]
+    amp, amperror = re.findall("amp.*(\d+\.\d+).*(\d+\.\d+)", txt)[0]
+    qual = re.findall("Qual (\d+\.\d+)", txt)[0]
+    fitcomment = "# fit({},{}) m={} e={} qual:{}".format(ts, te, mass, masserror, qual)
+    print fitcomment,
+    print args.filetoappend.read(),
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="append fit result (to an emass file)")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="increase output verbosity")
+    parser.add_argument('fitlog', type=argparse.FileType('r'))
+    parser.add_argument('filetoappend', type=argparse.FileType('r'))
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+        logging.debug("Verbose debuging mode activated")
+    else:
+        logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+
+    append_fit_results()
