2a0c21c07d097a4fbf648f7d77b6d44e9a0b7151
Brendan Fahy <bmfahy@gmail.com>
2014-03-31 21:28:14 -0400

    added format op name to help with translating colin format names to david foramt

---
+++ format_opname.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+import sys
+import re
+import logging
+import argparse
+
+def format_op(op):
+    "format operator from ruby format to david format"
+    base, rep, p1,c1,o1, p2,c2,o2 = re.match('@oplist.push\("(.*)"\)', op).group(1).split()
+
+    iso, n1, n2 =  base.split("_")
+    p1 = "".join(p1.replace("[P=(", "").replace(")", "").split(","))
+    p2 = "".join(p2.replace("[P=(", "").replace(")", "").split(","))
+    o1=o1.strip(']\"').replace("_","")
+    o2=o2.strip(']\"').replace("_","")
+
+    print "{}{}-{}p{}{}{}-{}p{}{}{}".format(iso, rep, n1, p1, o1, c1, n2, p2, o2, c2)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="translate ruby format to colin format")
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+    parser.add_argument('strings', metavar='s', type=str, nargs='+',
+                        help='expected level to determine opname from')
+    args = parser.parse_args()
+
+    if args.strings == ["-"]:
+        args.strings = []
+        for line in sys.stdin:
+            args.strings.append( line.strip())
+
+    for op in args.strings:
+        format_op(op)
