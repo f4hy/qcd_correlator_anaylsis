34d741174bb29b1c9ecb490dc0d577b6f626b685
Brendan Fahy <bmfahy@gmail.com>
2015-06-03 17:24:01 +0900

    have netwon method not throw warnings

--- newton.py
+++ newton.py
@@ -2,6 +2,7 @@
 import numpy as np
 import scipy.optimize
 import logging
+import warnings
 
 def newton_cosh_for_m(i,j,ave_cor, guess,T):
 
@@ -10,14 +11,15 @@ def newton_cosh_for_m(i,j,ave_cor, guess,T):
         denominator = np.exp(-m*j) + np.exp(-1.0*m*(T-j))
         return numerator/denominator - ave_cor[i]/ave_cor[j]
 
-
     logging.debug("newtons method to find cosh (T={}) emass starting guess {}, {},{}".format(T,guess, i, j))
-    try:
-        result = scipy.optimize.newton(f,guess)
-    except RuntimeError:
-        logging.error("Newtons failed to converge using standard for {},{}".format(i,j))
-        return guess
-    logging.debug("newtons method converged to {}".format(result))
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        try:
+            result = scipy.optimize.newton(f,guess, tol=1.48e-6)
+        except RuntimeError:
+            logging.error("Newtons failed to converge using standard for {},{}".format(i,j))
+            return guess
+        logging.debug("newtons method converged to {}".format(result))
     return result
 
 def newton_sinh_for_m(i,j,ave_cor, guess,T):
