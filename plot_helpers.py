e7ecd2b6aae904c9c251cdf29c47e0cd3b5cbff1
Brendan Fahy <bmfahy@gmail.com>
2015-12-22 11:06:02 +0900

    printing paren formated errors

--- plot_helpers.py
+++ plot_helpers.py
@@ -16,3 +16,23 @@ def ncols(N):
 def auto_fit_range(minval, maxval):
     spread = maxval - minval
     return minval-spread*0.1, maxval+spread*0.1
+
+def print_paren_error(value,error):
+
+    try:
+        digits = math.floor(math.log10(error))
+    except ValueError:
+        return value
+    if digits >= 1.0:
+        formated_string = "{m:d}({e:d})".format(m=int(value), e=int(error))
+        return formated_string
+    if digits == 0.0:
+        formated_string = "{m:0.1f}({e:.1f})".format(m=value, e=error)
+        return formated_string
+
+    digits= -1.0*digits
+    formated_error = int(round(error * (10**(digits + 1))))
+    formated_value = "{m:.{d}f}".format(d=int(digits) + 1, m=value)
+
+    formated_string = "{m}({e})".format(m=formated_value, e=formated_error)
+    return formated_string
