24af432a0b4249a74a843fe871292723c1f1ea98
Brendan Fahy <bmfahy@gmail.com>
2016-01-19 14:26:04 +0900

    better logging on newtons

--- newton.py
+++ newton.py
@@ -4,37 +4,42 @@ import scipy.optimize
 import logging
 import warnings
 
-def newton_cosh_for_m(i,j,ave_cor, guess,T):
+
+def newton_cosh_for_m(i, j, ave_cor, guess, T):
 
     def f(m):
         numerator = np.exp(-m*i) + np.exp(-1.0*m*(T-i))
         denominator = np.exp(-m*j) + np.exp(-1.0*m*(T-j))
         return numerator/denominator - ave_cor[i]/ave_cor[j]
 
-    logging.debug("newtons method to find cosh (T={}) emass starting guess {}, {},{}".format(T,guess, i, j))
+    logging.debug("newtons method to find cosh (T={}) emass starting guess"
+                  " {}, {},{}, fguess={}".format(T, guess, i, j, f(guess)))
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         try:
-            result = scipy.optimize.newton(f,guess, tol=1.48e-6)
+            result = scipy.optimize.newton(f, guess, tol=1.48e-6)
         except RuntimeError:
-            logging.error("Newtons failed to converge using standard for {},{}".format(i,j))
+            logging.error("Newtons failed to converge using standard for {},{}".format(i, j))
+            logging.error("Newtons failed, fallback is {}".format(guess))
             return guess
-        logging.debug("newtons method converged to {}".format(result))
+        logging.debug("newtons method converged to {} from {}".format(result, guess))
     return result
 
-def newton_sinh_for_m(i,j,ave_cor, guess,T):
+
+def newton_sinh_for_m(i, j, ave_cor, guess, T):
 
     def f(m):
         numerator = np.exp(-m*i) - np.exp(-1.0*m*(T-i))
         denominator = np.exp(-m*j) - np.exp(-1.0*m*(T-j))
         return numerator/denominator - ave_cor[i]/ave_cor[j]
 
-
-    logging.debug("newtons method to find sinh (T={}) emass starting guess {}, {},{}".format(T,guess, i, j))
+    logging.debug("newtons method to find sinh (T={}) emass starting guess "
+                  "{}, {},{}".format(T, guess, i, j))
     try:
-        result = scipy.optimize.newton(f,guess)
+        result = scipy.optimize.newton(f, guess)
     except RuntimeError:
-        logging.error("Newtons failed to converge using standard for {},{}".format(i,j))
+        logging.error("Newtons failed to converge using standard for {},{}".format(i, j))
+        logging.error("Newtons failed, fallback is {}".format(guess))
         return guess
     logging.debug("newtons method converged to {}".format(result))
     return result
